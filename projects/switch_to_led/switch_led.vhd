2f80d8e28aa0db8b0acaa2ca1d296c7baa73954b
Harinandan A M <116416113+HarinandanAM@users.noreply.github.com>
2024-09-05 20:39:51 +0530

    Update and rename lab1_1.vhd to switch_led.vhd

--- projects/switch_to_led/switch_led.vhd
+++ projects/switch_to_led/switch_led.vhd
@@ -1,11 +1,11 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
-ENTITY lab1_1 IS
+ENTITY switch_led IS
 PORT(mySW : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
      myLEDR : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
     );
-END lab1_1;
-ARCHITECTURE Behavior OF lab1_1 IS
+END switch_led;
+ARCHITECTURE Behavior OF switch_led IS
 BEGIN
 	myLEDR <= mySW;
 END Behavior;
