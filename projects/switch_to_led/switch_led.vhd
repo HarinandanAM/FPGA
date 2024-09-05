b1f6243bb34d23644ed63efc214933afcc586691
Harinandan A M <116416113+HarinandanAM@users.noreply.github.com>
2024-09-05 20:45:47 +0530

    Update switch_led.vhd

--- projects/switch_to_led/switch_led.vhd
+++ projects/switch_to_led/switch_led.vhd
@@ -1,11 +1,13 @@
-LIBRARY ieee ;
-USE ieee.std_logic_1164.all ;
-ENTITY switch_led IS
-PORT(mySW : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-     myLEDR : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
-    );
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY switch_led IS  
+  PORT(mySW : IN STD_LOGIC_VECTOR(9 DOWNTO 0);         -- Input: Switches (10-bit)
+       myLEDR : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)       -- Output: LEDs (10-bit)
+  );
 END switch_led;
+
 ARCHITECTURE Behavior OF switch_led IS
-BEGIN
-	myLEDR <= mySW;
+ BEGIN
+  myLEDR <= mySW;
 END Behavior;
